0114d2af7166dad5bef75b14f76aca75d9edc0c2
prznoc <przemeknok@interia.pl>
2020-06-06 14:54:12 +0200

    Code generation part 1

---
+++ drone.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void user_readweight_spg(){
+    prinft("user_readweight_spg");
+}
+
+void user_readheight_spg(){
+    prinft("user_readheight_spg");
+}
+
+void user_rotorpower_spg(){
+    prinft("user_rotorpower_spg");
+}
+
+void user_scanarea_spg(){
+    prinft("user_scanarea_spg");
+}
+
+void user_gpsdata_spg(){
+    prinft("user_gpsdata_spg");
+}
+
+void user_setnewtarget_spg(){
+    prinft("user_setnewtarget_spg");
+}
+
+void user_analysedata_spg(){
+    prinft("user_analysedata_spg");
+}
