5be8f2ddc406f603670ac939773d92fb004bd328
prznoc <przemeknok@interia.pl>
2020-06-12 23:10:04 +0200

    remove operator

--- drone.c
+++ drone.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void user_readweight_spg(int weight_in, float weight_out){
+void user_readweight_spg(int weight_in, float* weight_out){
     //prinft("user_readweight_spg");
 }
 
-void user_readheight_spg(float height_in, float height_out){
+void user_readheight_spg(float height_in, float* height_out){
     //prinft("user_readheight_spg");
 }
 
-void user_rotorpower_spg(float rotor_power_in, float rotor_power_out){
+void user_rotorpower_spg(float rotor_power_in, float* rotor_power_out){
     //prinft("user_rotorpower_spg");
 }
 
@@ -16,14 +16,14 @@ void user_scanarea_spg(){
     //prinft("user_scanarea_spg");
 }
 
-void user_gpsdata_spg(float location_in, float location_out){
+void user_gpsdata_spg(float location_in, float* location_out){
     //prinft("user_gpsdata_spg");
 }
 
-void user_setnewtarget_spg(float weight_in, float weight_out){
+void user_setnewtarget_spg(float new_target_in, float* new_target_out){
     //prinft("user_setnewtarget_spg");
 }
 
-void user_analysedata_spg(float location_in, float height_in, float weight_in, float rotor_power_out, float rotor_power_in, float new_target){
+void user_analysedata_spg(float location_in, float height_in, float weight_in, float* rotor_power_out, float rotor_power_in, float new_target){
     //prinft("user_analysedata_spg");
 }
