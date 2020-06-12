d14f17bee5aaef4eccd63a73dd19033af45ffaea
prznoc <przemeknok@interia.pl>
2020-06-12 22:20:45 +0200

    Code generation

--- drone.c
+++ drone.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
-void user_readweight_spg(){
-    prinft("user_readweight_spg");
+void user_readweight_spg(int weight_in, float weight_out){
+    //prinft("user_readweight_spg");
 }
 
-void user_readheight_spg(){
-    prinft("user_readheight_spg");
+void user_readheight_spg(float height_in, float height_out){
+    //prinft("user_readheight_spg");
 }
 
-void user_rotorpower_spg(){
-    prinft("user_rotorpower_spg");
+void user_rotorpower_spg(float rotor_power_in, float rotor_power_out){
+    //prinft("user_rotorpower_spg");
 }
 
 void user_scanarea_spg(){
-    prinft("user_scanarea_spg");
+    //prinft("user_scanarea_spg");
 }
 
-void user_gpsdata_spg(){
-    prinft("user_gpsdata_spg");
+void user_gpsdata_spg(float location_in, float location_out){
+    //prinft("user_gpsdata_spg");
 }
 
-void user_setnewtarget_spg(){
-    prinft("user_setnewtarget_spg");
+void user_setnewtarget_spg(float weight_in, float weight_out){
+    //prinft("user_setnewtarget_spg");
 }
 
-void user_analysedata_spg(){
-    prinft("user_analysedata_spg");
+void user_analysedata_spg(float location_in, float height_in, float weight_in, float rotor_power_out, float rotor_power_in, float new_target){
+    //prinft("user_analysedata_spg");
 }
