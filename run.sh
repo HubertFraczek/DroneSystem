6e0fbdb7eee2ba203e2838ca39ac780a8c7dd355
HubertFraczek <hhubcio12@gmail.com>
2020-06-14 13:58:28 +0200

    removed faulty ports and connections, script for fixing errors

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sed -i 's/the_aerial_global_location_out/thread_gps_global_location_out/g' drone_impl/the_controller/activity.c &
+
+sed -i 's/the_drone_rotor_global_rotor_power_in/thread_rotor_global_rotor_power_in/g' drone_impl/the_controller/activity.c &
+
+drone_impl/make
