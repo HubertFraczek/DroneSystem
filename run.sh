b9eed38a3fee27b686de3f6b303a3f76f320f192
HubertFraczek <hhubcio12@gmail.com>
2020-06-14 14:41:46 +0200

    make command

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ sed -i 's/the_aerial_global_location_out/thread_gps_global_location_out/g' drone
 
 sed -i 's/the_drone_rotor_global_rotor_power_in/thread_rotor_global_rotor_power_in/g' drone_impl/the_controller/activity.c &
 
-drone_impl/make
+(cd drone_impl/ ; make)
